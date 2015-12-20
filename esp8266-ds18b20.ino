cc3f81ad995d9df9bbad751da2c345c2b1aaeb8f
hvdwolf <hvdwolf@gmail.com>
2015-12-20 15:49:11 +0100

    First commit of a DS18B20 version

---
+++ esp8266-ds18b20.ino
@@ -0,0 +1,317 @@
+#include <Time.h>
+#include <ESP8266mDNS.h>
+#include <OneWire.h> // http://www.pjrc.com/teensy/arduino_libraries/OneWire.zip
+#include <DallasTemperature.h> // http://download.milesburton.com/Arduino/MaximTemperature/DallasTemperature_LATEST.zip
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <Base64.h>
+#include <EEPROM.h>
+
+
+ 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////        !!PLEASE CHANGE THESE!!
+String ssid		= "WiFi SSID";
+String password	= "WiFi Password";
+
+String espName    = "esp12";
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////        NETWORK
+ESP8266WebServer  server(80);
+MDNSResponder   mdns;
+
+const char* APssid = "ESPap";
+const char* APpassword = "123456789";
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////         GLOBAL VARIABLES
+long    lastInterval  = 0;
+const int httpPort    = 80;
+float   temperature   = 0.0;
+
+String deviceType   = "DS18B20";
+long sendInterval   = 30000; //in millis
+
+String Username     = "ESPap";
+String Password     = "123456789";
+
+char authVal[40];  
+char authValEncoded[40];
+
+String host   = "192.168.0.100";
+
+String ClientIP;
+// send data
+WiFiClient client;
+
+
+// Define pins
+#define ONE_WIRE_BUS 2 // DS18B20 PIN GPIO2
+
+// Start includes
+OneWire oneWire(ONE_WIRE_BUS); // Setup a oneWire instance
+DallasTemperature sensors(&oneWire); // Pass our oneWire reference to Dallas Temperature
+DeviceAddress Sump;
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////         HTML SNIPPLETS
+String header       =  "<html lang='en'><head><title>ESP8266 Pimatic Client</title><meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1'><link rel='stylesheet' href='http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css'><script src='https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js'></script><script src='http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js'></script></head><body>";
+String navbar       =  "<nav class='navbar navbar-default'><div class='container-fluid'><div class='navbar-header'><a class='navbar-brand' href='/'>ESP8266 Pimatic Client</a></div><div><ul class='nav navbar-nav'><li class='active'><a href='/'><span class='glyphicon glyphicon-dashboard'></span> Status</a></li><li class='dropdown'><a class='dropdown-toggle' data-toggle='dropdown' href='#'><span class='glyphicon glyphicon-cog'></span> Configure<span class='caret'></span></a><ul class='dropdown-menu'><li><a href='/cliconf'>Client</a></li><li><a href='/serconf'>Server</a></li></ul></li></ul></div></div></nav>  "; 
+String navbarNonActive    = "<nav class='navbar navbar-default'><div class='container-fluid'><div class='navbar-header'><a class='navbar-brand' href='/'>ESP8266 Pimatic Client</a></div><div><ul class='nav navbar-nav'><li><a href='/'><span class='glyphicon glyphicon-dashboard'></span> Status</a></li><li class='dropdown'><a class='dropdown-toggle' data-toggle='dropdown' href='#'><span class='glyphicon glyphicon-cog'></span> Configure<span class='caret'></span></a><ul class='dropdown-menu'><li><a href='/cliconf'>Client</a></li><li><a href='/serconf'>Server</a></li></ul></li></ul></div></div></nav>  ";
+String containerStart   =  "<div class='container'><div class='row'>";
+String containerEnd     =  "<div class='clearfix visible-lg'></div></div></div>";
+String siteEnd        =  "</body></html>";
+  
+String panelHeaderName    =  "<div class='col-md-4'><div class='page-header'><h1>";
+String panelHeaderEnd   =  "</h1></div>";
+String panelEnd       =  "</div>";
+  
+String panelBodySymbol    =  "<div class='panel panel-default'><div class='panel-body'><span class='glyphicon glyphicon-";
+String panelBodyName    =  "'></span> ";
+String panelBodyValue   =  "<span class='pull-right'>";
+String panelBodyEnd     =  "</span></div></div>";
+
+String inputBodyStart   =  "<form action='' method='POST'><div class='panel panel-default'><div class='panel-body'>";
+String inputBodyName    =  "<div class='form-group'><div class='input-group'><span class='input-group-addon' id='basic-addon1'>";
+String inputBodyPOST    =  "</span><input type='text' name='";
+String inputBodyClose   =  "' class='form-control' aria-describedby='basic-addon1'></div></div>";
+String inputBodyEnd     =  "</div><div class='panel-footer clearfix'><div class='pull-right'><button type='submit' class='btn btn-default'>Send</button></div></div></div></form>";
+
+
+//String landingNav     = "<nav class='navbar navbar-default'> <div class='container-fluid'> <div class='navbar-header'> <a class='navbar-brand' href='/'>ESP8266 Pimatic Client</a> </div></div></nav><br><br><br>";
+//String landingStartPartA  = "<div class='container'> <div class='row' > <div class='col-md-offset-3 col-md-6'> <div class='panel panel-default'> <div class='panel-heading'> Please login to your WiFi Network </div><div class='panel-body'> <form method='post' action=''> <div class='form-group'><div class='input-group'><span class='input-group-addon' id='basic-addon1'>Wifi Name </span><input type='text' class='form-control' placeholder='Enter SSID' aria-describedby='basic-addon1' name='wifiname'></div></div>";
+//String landingStartPartB    =   "<div class='form-group'><div class='input-group'><span class='input-group-addon' id='basic-addon1'>WiFi Pass </span><input type='password' class='form-control' placeholder='Password' aria-describedby='basic-addon1' name='wifipass'></div></div><div class='form-group'><div class='input-group'><span class='input-group-addon' id='basic-addon1'>Device Name ";
+//String landingStartPartC      =    "</span><input type='text' class='form-control' placeholder='Name your device e.g. kitchen' aria-describedby='basic-addon1' name='devicename'></div></div><a class='btn btn-primary' data-toggle='collapse' href='#collapseExample' aria-expanded='false' aria-controls='collapseExample'> Available Networks </a> <div class='pull-right'> <button type='submit' class='btn btn-default'>Send</button> </div></form> <div class='collapse' id='collapseExample'> <div class='well'>";
+//String landingEnd     = "</div></div></div></div></div></div></body></html>";
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////         ROOT 
+void handle_root() {
+
+  Serial.println(server.args());
+  
+  // get IP
+  IPAddress ip = WiFi.localIP();
+  ClientIP = String(ip[0]) + '.' + String(ip[1]) + '.' + String(ip[2]) + '.' + String(ip[3]);
+  delay(500);
+  
+  String title1     = panelHeaderName + String("Sensor Data") + panelHeaderEnd;
+  String Temperature    = panelBodySymbol + String("fire") + panelBodyName + String("Temperature") + panelBodyValue + temperature + String("°C") + panelBodyEnd + panelEnd;
+  
+  String title2     = panelHeaderName + String("Client Settings") + panelHeaderEnd;
+  String IPAddClient    = panelBodySymbol + String("globe") + panelBodyName + String("IP Address") + panelBodyValue + ClientIP + panelBodyEnd;
+  String DeviceType   = panelBodySymbol + String("scale") + panelBodyName + String("Device Type") + panelBodyValue + deviceType + panelBodyEnd;
+  String ClientName   = panelBodySymbol + String("tag") + panelBodyName + String("Client Name") + panelBodyValue + espName + panelBodyEnd;
+  String Interval   = panelBodySymbol + String("hourglass") + panelBodyName + String("Interval") + panelBodyValue + sendInterval + String(" millis") + panelBodyEnd;
+  String Uptime     = panelBodySymbol + String("time") + panelBodyName + String("Uptime") + panelBodyValue + hour() + String(" h ") + minute() + String(" min ") + second() + String(" sec") + panelBodyEnd + panelEnd;
+  
+  String title3     = panelHeaderName + String("Server Settings") + panelHeaderEnd;
+  String IPAddServ    = panelBodySymbol + String("globe") + panelBodyName + String("IP Address") + panelBodyValue + host + panelBodyEnd;
+  String User     = panelBodySymbol + String("user") + panelBodyName + String("Username") + panelBodyValue + Username + panelBodyEnd + panelEnd;
+  
+  
+  //String data = title1 + Humidity + Temperature + title2 + IPAddClient + DeviceType + ClientName + Interval + Uptime + title3 + IPAddServ + User;
+  //server.send ( 200, "text/html", header + navbar + containerStart + data + containerEnd + siteEnd );
+   server.send ( 200, "text/html", header + navbar + containerStart + title1 + Temperature + title2 + IPAddClient + DeviceType + ClientName + Interval + Uptime + title3 + IPAddServ + User + containerEnd + siteEnd);
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////         CONFIG - Client
+void handle_cliconf() {
+
+  String payload=server.arg("name");
+  if (payload.length() > 0 ) {
+    espName = payload;
+  }
+  Serial.println(payload);
+
+  payload=server.arg("type");
+  if (payload.length() > 0 ) {
+    deviceType = payload;
+  }
+  Serial.println(payload);
+
+  payload=server.arg("interval");
+  if (payload.length() > 0 ) {
+    sendInterval = payload.toInt();
+  }
+  Serial.println(payload);
+  
+  String title1 = panelHeaderName + String("Client Configuration") + panelHeaderEnd; 
+  
+  String data = title1 + inputBodyStart + inputBodyName + String("Name") + inputBodyPOST + String("name") + inputBodyClose + inputBodyName + String("Device Type") + inputBodyPOST + String("type") + inputBodyClose + inputBodyName + String("Interval in seconds") + inputBodyPOST + String("interval") + inputBodyClose + inputBodyEnd;
+  server.send ( 200, "text/html", header + navbarNonActive + containerStart + data + containerEnd + siteEnd );
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////         CONFIG - Server
+void handle_serconf() {
+  
+  String payload=server.arg("server");
+  if (payload.length() > 0 ) {
+    host = payload;
+  }
+  Serial.println(payload);
+  
+  payload=server.arg("user");
+  if (payload.length() > 0 ) {
+    Username = payload;
+  }
+  Serial.println(payload);
+
+  payload=server.arg("password");
+  if (payload.length() > 0 ) {
+    Password = payload;
+  }
+  Serial.println(payload);
+  
+  String title1 = panelHeaderName + String("Server Configuration") + panelHeaderEnd;
+  
+  String data = title1 + inputBodyStart + inputBodyName + String("Pimatic Server") + inputBodyPOST + String("server")  + inputBodyClose + inputBodyName + String("Username") + inputBodyPOST + String("user") + inputBodyClose + inputBodyName + String("Password") + inputBodyPOST + String("password") + inputBodyClose + inputBodyEnd;
+  
+  server.send ( 200, "text/html", header + navbarNonActive + containerStart + data + containerEnd + siteEnd);
+  
+  
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////         LANDING
+/*
+void landing() {
+  
+  String payload=server.arg("wifiname");
+  if (payload.length() > 0 ) {
+    ssid = payload;
+  }
+  Serial.println(payload);
+  payload=server.arg("wifipass");
+  if (payload.length() > 0 ) {
+    password = payload;
+  }
+  Serial.println(payload);
+  
+  payload=server.arg("devicename");
+  if (payload.length() > 0 ) {
+    espName = payload;
+  }
+  Serial.println(payload);
+  
+  String landing = header + landingNav + landingStartPartA + landingStartPartB + landingStartPartC + landingEnd;
+  server.send ( 200, "text/html", landing);
+}
+*/
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////         SEND DATA
+void send_data() {
+  
+  String yourdata;
+  
+  // Read DS18B20 and transmit value as sensor 1
+  float t;
+
+  sensors.begin(); //start up temp sensor
+  delay(400); //Wait for newly restarted system to stabilize
+  (!sensors.getAddress(Sump, 0));
+  sensors.setResolution(Sump, 11);
+ 
+  sensors.requestTemperatures(); // Get the temperature
+  t = sensors.getTempCByIndex(0); // Get temperature in Celcius
+
+  //check errors
+  if (isnan(t)) {
+    Serial.println("Failed to read from DS18B20 sensor!");
+    return;
+  }
+
+  temperature = t;
+     
+  char host_char_array[host.length()+1];
+  host.toCharArray(host_char_array,host.length()+1);
+  
+  if (!client.connect(host_char_array, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+  
+  // calculate Base64Login
+  memset(authVal,0,40);
+  (Username + String(":") + Password).toCharArray(authVal, 40);
+  memset(authValEncoded,0,40);
+  base64_encode(authValEncoded, authVal, (Username + String(":") + Password).length());
+  
+//  char base64login[40];
+  
+//  (Username + String(":") + Password).toCharArray(base64login, 40);
+  
+  //Send Temperature
+  yourdata = "{\"type\": \"value\", \"valueOrExpression\": \"" + String(temperature) + "\"}";
+    
+    client.print("PATCH /api/variables/");
+    client.print(espName);
+    client.print("-temp HTTP/1.1\r\n");
+    client.print("Authorization: Basic ");
+    client.print(authValEncoded);
+    client.print("\r\n");
+    client.print("Host: " + host +"\r\n");
+    client.print("Content-Type:application/json\r\n");
+    client.print("Content-Length: ");
+    client.print(yourdata.length());
+    client.print("\r\n\r\n");
+    client.print(yourdata);
+  
+  
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////         AP
+void setupAP(void) {
+  delay(100);
+  WiFi.softAP(APssid,APpassword,6);
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////         SETUP 
+void setup(void)
+{
+  Serial.begin(115200);
+ 
+   
+      WiFi.begin(ssid.c_str(), password.c_str());
+      while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+      } 
+      Serial.println("");
+      Serial.print("Connected to ");
+      Serial.println(ssid);
+      Serial.print("IP address: ");
+      Serial.println(WiFi.localIP());
+      
+      server.on("/", handle_root);
+  
+      server.on("/cliconf", handle_cliconf);
+  
+      server.on("/serconf", handle_serconf);
+
+      //server.on("/landing", landing);
+  
+      if (!mdns.begin(espName.c_str(), WiFi.localIP())) {
+        Serial.println("Error setting up MDNS responder!");
+        while(1) { 
+          delay(1000);
+        }
+      }
+      server.begin();
+      Serial.println("HTTP server started");
+
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////           MAIN 
+void loop(void)
+{
+  
+    if (millis() - lastInterval > sendInterval) {
+      send_data();
+      lastInterval = millis();
+    }
+   
+  server.handleClient();
+  
+} 
+
